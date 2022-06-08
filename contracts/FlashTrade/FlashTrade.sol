22c3b68ba928e50dad8574fc9fa3eddc0c1e124c
Mikhail <yaetiya@gmail.com>
2022-06-08 12:46:20 +0300

    flash trade init

---
+++ contracts/FlashTrade/FlashTrade.sol
@@ -0,0 +1,71 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+abstract contract Token is ERC20 {
+    function mint(address to, uint256 amount) public virtual;
+
+    function burn(address to, uint256 amount) public virtual;
+}
+
+abstract contract Trade {
+    event SellEvent(address from, uint256 amount, address tokenAddress);
+    event BuyEvent(address from, uint256 amount, address tokenAddress);
+    mapping(address => uint256) public currentPrices;
+
+    function buy(address contractAddress)
+        external
+        payable
+        virtual
+        returns (uint256);
+
+    function sell(address contractAddress, uint256 amount)
+        external
+        virtual
+        returns (uint256);
+}
+
+contract FlashTrade {
+    uint256 public xltBound = 0.1 ether;
+    address public pjAddress;
+    address public xrc20Address;
+    address public owner;
+
+    address public tradeContractProxyddress;
+
+    //  = 0x916Bb4C1960Abb890CC053d00e685f21EA30fC58;
+
+    constructor(
+        address _pjAddress,
+        address _xrc20Address,
+        address _tradeContractProxyddress
+    ) {
+        tradeContractProxyddress = _tradeContractProxyddress;
+        owner = msg.sender;
+        pjAddress = _pjAddress;
+        xrc20Address = _xrc20Address;
+    }
+
+    function buy(uint256 xltAmount, address buyerAddress) external {
+        require(xltAmount > xltBound, "xlt amount is too small");
+        Trade trade = Trade(tradeContractProxyddress);
+        uint256 boughtXRC20 = trade.buy{value: xltAmount - xltBound}(
+            xrc20Address
+        );
+        Token xrc20 = Token(xrc20Address);
+        xrc20.transfer(buyerAddress, boughtXRC20);
+        payable(buyerAddress).transfer(xltBound);
+    }
+
+    function withdraw() external {
+        require(msg.sender == owner || msg.sender == pjAddress);
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    event Received(address, uint256);
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+}
---
+++ test/flashTrade.ts
@@ -0,0 +1,59 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { ethers, waffle } from "hardhat";
+
+let xrc20Contract, xrc20: Contract;
+let tradeContract, trade: Contract;
+let flashTradeContract, flashTrade: Contract;
+
+let owner: SignerWithAddress;
+let addr1: SignerWithAddress;
+let addr2: SignerWithAddress;
+
+before(async function () {
+  tradeContract = await ethers.getContractFactory("ExposedTrade");
+  [owner, addr1, addr2] = await ethers.getSigners();
+  trade = await tradeContract.deploy();
+
+  xrc20Contract = await ethers.getContractFactory("XRC20");
+  xrc20 = await xrc20Contract.deploy(trade.address);
+
+  flashTradeContract = await ethers.getContractFactory("FlashTrade");
+  flashTrade = await flashTradeContract.deploy(
+    addr1.address,
+    xrc20.address,
+    trade.address
+  );
+});
+
+describe("Deploy FlashTrade", function () {
+  it("Deploy", async function () {
+    await flashTrade.deployed();
+  });
+});
+
+describe("Operations", function () {
+  it("Buy for all", async function () {
+    await owner.sendTransaction({
+      to: flashTrade.address,
+      value: ethers.utils.parseEther("10.0"),
+    });
+
+    await flashTrade.buy(ethers.utils.parseEther("10.0"), addr2.address);
+    expect(await waffle.provider.getBalance(flashTrade.address)).to.equal(0);
+
+    expect(await xrc20.balanceOf(addr2.address)).not.equal(0);
+  });
+  it("Withdraw", async function () {
+    await owner.sendTransaction({
+      to: flashTrade.address,
+      value: ethers.utils.parseEther("10.0"),
+    });
+    expect(await waffle.provider.getBalance(flashTrade.address)).to.equal(ethers.utils.parseEther("10.0"));
+
+    await flashTrade.withdraw();
+    expect(await waffle.provider.getBalance(flashTrade.address)).to.equal(0);
+    expect(await waffle.provider.getBalance(owner.address)).not.equal(0);
+  });
+});
